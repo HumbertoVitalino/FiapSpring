446ddcf606763bc7ac7eb2b85d7c5fcf404e7c30
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-17 10:45:39 -0300

    refactor: contactapp

--- src/main/java/br/com/fiap/contacts/ContactApp.java
+++ src/main/java/br/com/fiap/contacts/ContactApp.java
@@ -1,5 +1,7 @@
 package br.com.fiap.contacts;
 
+import br.com.fiap.contacts.dao.Connection;
+import br.com.fiap.contacts.dao.ContactDao;
 import br.com.fiap.contacts.model.Contact;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -9,16 +11,47 @@ import java.time.LocalDate;
 
 public class ContactApp {
     public static void main(String[] args) {
-        Contact contact = new Contact();
-        contact.setName("John Doe");
-        contact.setEmail("john@doe.com");
-        contact.setDateOfBirth(LocalDate.of(1990, 1, 1));
+        EntityManager em = Connection.getEntityManager();
+        //create(em);
+        //update(em);
+        delete(em);
+    }
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("contacts");
-        EntityManager em = emf.createEntityManager();
+    public static void create(EntityManager em) {
+        Contact contact = new Contact();
+        contact.setName("Gabriela Ribeiro");
+        contact.setEmail("gabriela@ribeiro.com");
+        contact.setDateOfBirth(LocalDate.of(2003, 7, 7));
+
+        ContactDao dao = new ContactDao(em);
 
         em.getTransaction().begin();
-        em.persist(contact);
+        dao.save(contact);
+        em.getTransaction().commit();
+    }
+
+    public static void update(EntityManager em) {
+        Contact contact = new Contact();
+        contact.setId(3L);
+        contact.setName("Gabriela Ribeiro");
+        contact.setEmail("gabriela.ribeiro@gmail.com");
+        contact.setDateOfBirth(LocalDate.of(2003, 7, 7));
+
+        ContactDao dao = new ContactDao(em);
+
+        em.getTransaction().begin();
+        dao.update(contact);
+        em.getTransaction().commit();
+    }
+
+    public static void delete(EntityManager em) {
+        Contact contact = new Contact();
+        contact.setId(3L);
+
+        ContactDao dao = new ContactDao(em);
+
+        em.getTransaction().begin();
+        dao.delete(contact);
         em.getTransaction().commit();
     }
 }
