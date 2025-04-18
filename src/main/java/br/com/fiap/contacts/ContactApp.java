61c05f2703806292822ba0c4a51e1eb48b306d10
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-18 10:12:33 -0300

    feat: new contact dao methods

--- src/main/java/br/com/fiap/contacts/ContactApp.java
+++ src/main/java/br/com/fiap/contacts/ContactApp.java
@@ -4,24 +4,45 @@ import br.com.fiap.contacts.dao.Connection;
 import br.com.fiap.contacts.dao.ContactDao;
 import br.com.fiap.contacts.model.Contact;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class ContactApp {
     public static void main(String[] args) {
         EntityManager em = Connection.getEntityManager();
         //create(em);
+        //create(em);
+        //create(em);
         //update(em);
         //delete(em);
-        get(em, 5);
+        //get(em, 5);
+        //getAll(em);
+        getByEmail(em, "humberto@gmail.com");
+    }
+
+    private static void getAll(EntityManager em) {
+        ContactDao dao = new ContactDao(em);
+
+        List<Contact> contacts = dao.getAll();
+        for(Contact contact : contacts) {
+            System.out.println(contact.toString());
+        }
+    }
+
+    public static void getByEmail(EntityManager em, String email) {
+        ContactDao dao = new ContactDao(em);
+
+        List<Contact> contacts = dao.getByEmail(email);
+        for(Contact contact : contacts) {
+            System.out.println(contact.toString());
+        }
     }
 
     public static void create(EntityManager em) {
         Contact contact = new Contact();
-        contact.setName("Gabriela Ribeiro");
-        contact.setEmail("gabriela@ribeiro.com");
+        contact.setName("Humberto Ribeiro");
+        contact.setEmail("humberto@gmail.com");
         contact.setDateOfBirth(LocalDate.of(2003, 7, 7));
 
         ContactDao dao = new ContactDao(em);
