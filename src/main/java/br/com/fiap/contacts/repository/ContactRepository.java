fdd199c48854f622117906664e1dfcbe8e7b137d
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-19 23:46:24 -0300

    feat: repository

---
+++ src/main/java/br/com/fiap/contacts/repository/ContactRepository.java
@@ -0,0 +1,14 @@
+package br.com.fiap.contacts.repository;
+
+import br.com.fiap.contacts.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    public Contact findByName(String name);
+
+    public List<Contact> findByBirthdayBetween(LocalDate startDate, LocalDate endDate);
+}
