5714a1174a0e538fca2f63446b87e94300974a9f
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-24 08:28:10 -0300

    feat: securityConfig

--- src/main/java/br/com/fiap/contacts/config/security/SecurityConfig.java
+++ src/main/java/br/com/fiap/contacts/config/security/SecurityConfig.java
@@ -1,16 +1,43 @@
 package br.com.fiap.contacts.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        return null;
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/contacts").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/contacts")
+                        .hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated())
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
