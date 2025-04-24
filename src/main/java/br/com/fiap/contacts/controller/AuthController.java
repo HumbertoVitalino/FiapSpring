8bb808a39c7590839c9c07754d544fbc31a20b4a
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-24 08:28:18 -0300

    feat: auth controller

---
+++ src/main/java/br/com/fiap/contacts/controller/AuthController.java
@@ -0,0 +1,43 @@
+package br.com.fiap.contacts.controller;
+
+import br.com.fiap.contacts.dto.UserOutputDto;
+import br.com.fiap.contacts.dto.UserRegisterDto;
+import br.com.fiap.contacts.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid UserRegisterDto userDto) {
+        UsernamePasswordAuthenticationToken userPassword =
+                new UsernamePasswordAuthenticationToken(
+                        userDto.email(),
+                        userDto.password()
+                );
+        Authentication auth = authenticationManager.authenticate(userPassword);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserOutputDto create(@RequestBody @Valid UserRegisterDto userDto) {
+        UserOutputDto userRegister = null;
+        userRegister = service.create(userDto);
+        return userRegister;
+    }
+}
