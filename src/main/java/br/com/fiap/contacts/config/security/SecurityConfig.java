599ac7f01267e78a9fc157f1e115e55f391e7c42
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-23 21:24:54 -0300

    feat: security

---
+++ src/main/java/br/com/fiap/contacts/config/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package br.com.fiap.contacts.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return null;
+    }
+}
