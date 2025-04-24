9e6528223f6a31697d14104a5ffdfb37b87a7e60
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-23 21:25:27 -0300

    feat: repository + service

---
+++ src/main/java/br/com/fiap/contacts/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.fiap.contacts.repository;
+
+import br.com.fiap.contacts.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public UserDetails findByEmail(String email);
+}
