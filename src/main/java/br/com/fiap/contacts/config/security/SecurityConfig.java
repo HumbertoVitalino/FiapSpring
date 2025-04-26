fdd2ee585fbc524fb6fffab6b32c7a771498e628
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-26 11:11:05 -0300

    update config

--- src/main/java/br/com/fiap/contacts/config/security/SecurityConfig.java
+++ src/main/java/br/com/fiap/contacts/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.fiap.contacts.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private TokenVerifier verifier;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,11 +30,15 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/contacts").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/contacts").hasAnyRole("ADMIN", "USER")
                         .requestMatchers(HttpMethod.POST, "/api/contacts")
                         .hasRole("ADMIN")
                         .anyRequest()
                         .authenticated())
+                .addFilterBefore(
+                        verifier,
+                        UsernamePasswordAuthenticationFilter.class
+                )
                 .build();
     }
 
